6635b2fae6ed1a511f54d483ae6469401d7739d1
Gabriel Tumbaga <ubuntu@ip-172-26-4-97.us-west-2.compute.internal>
2019-04-15 07:07:48 +0000

    more progress.  got 1st set of real images in

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # omga.be - My Portfolio site
 
 Built with html/css, and NO JS! All features on here are done with css.
+
+inspired from https://jonny.me
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
   <style amp-custom>
+    <?php include_once('./css/reset.css'); ?>
+    <?php include_once('./css/normalize.css'); ?>
     <?php include_once('./css/style.css'); ?>
   </style>
   <script async src="https://cdn.ampproject.org/v0.js"></script>
@@ -38,13 +40,35 @@
     <div id="work">
       <div class="pod constrain">
         <div class="screens">
-          <div class="device laptop"><div class="inner">asdf</div></div>
-          <div class="device tablet"><div class="inner">asdf</div></div>
-          <div class="device smartphone"><div class="inner">asdf</div></div>
+          <div class="device laptop"><div class="inner"><amp-img alt="StagedHomes.com" src="/img/sites/stagedhomes/desktop.jpg" width="480" height="240" layout="responsive"></amp-img></div></div>
+          <div class="device tablet"><div class="inner"><amp-img alt="StagedHomes.com" src="/img/sites/stagedhomes/tablet.jpg" width="180" height="220" layout="responsive"></amp-img></div></div>
+          <div class="device smartphone"><div class="inner"><amp-img alt="StagedHomes.com" src="/img/sites/stagedhomes/smartphone.jpg" width="84" height="128" layout="responsive"></amp-img></div></div>
         </div>
         <div class="data">
-          <h3>Website Name</h3>
-          <p>This is the descriptive text</p>
+          <h3><a href="https://stagedhomes.com/" target="_blank">Staged Homes</a></h3>
+          <p>The World Leader in Home Staging Training</p>
+          <ul class="skills">
+            <li>PHP</li>
+            <li>Responsive</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
+      </div>
+     <hr class="constrain"/>
+      <div class="pod constrain">
+        <div class="screens">
+          <div class="device laptop"><div class="inner">480x240</div></div>
+          <div class="device tablet"><div class="inner">180x220</div></div>
+          <div class="device smartphone"><div class="inner">84x128</div></div>
+        </div>
+        <div class="data">
+          <h3>omga.be</h3>
+          <p>This is my website portfolio.</p>
+          <ul class="skills">
+            <li>AMP</li>
+            <li>Responsive</li>
+            <li>CSS Grid</li>
+          </ul>
         </div>
       </div>
      <hr class="constrain"/>
