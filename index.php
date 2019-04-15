75e21dfb00a6a8804811b4c17253693741654ea1
Gabriel Tumbaga <ubuntu@ip-172-26-4-97.us-west-2.compute.internal>
2019-04-15 01:57:27 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html amp>
+<head>
+  <meta charset="utf-8">
+  <link rel="canonical" href="/" />
+  <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+  <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+  <style amp-custom>
+    <?php include_once('./css/style.css'); ?>
+  </style>
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+</head>
+<body>
+  <header>
+    <div style="" id="bg-cover"></div>
+    <div class="top-bar">
+      <div class="brand">
+        <div class="name">Gabriel Tumbaga</div>
+        <div class="desc">Full Stack Web Developer</div>
+      </div>
+      <div class="nav-holder">
+      </div>
+    </div>
+  </header>
+  <main>
+    <div id="hero">
+      <h1>Full Stack Web Developer</h1>
+      <h2>LAMP Stack, MEAN Stack, MERN Stack</h2>
+      <button>Contact Me!</button>
+    </div>
+    Hello, AMP world.<br/>
+    My name is Gabe!
+  </main>
+</body>
+</html>
